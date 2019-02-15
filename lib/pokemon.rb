f42f8343174e73fbb3b720b8017721d5da37bde1
Victoria B <viktorija.bourassa@gmail.com>
2019-02-15 17:53:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,7 +24,7 @@ class Pokemon
     end
 
     def alter_hp(new_hp, db)
-       db.execute("UPDATE pokemon SET hp = ?", new_hp)
+       db.execute("UPDATE pokemon SET hp = new_hp WHERE hp = 60")
     end
 
 end
